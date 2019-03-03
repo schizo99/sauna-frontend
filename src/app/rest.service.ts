07a684a55fc64b6da8071288c3d855e36c893e06
Andreas Åhman <andreas.ahman@ikea.com>
2019-03-03 18:44:47 +0100

    Switch to remote backend

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
-const endpoint = 'http://localhost:3000/';
+const endpoint = 'http://schizo.local:3000/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
