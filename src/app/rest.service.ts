7991ae5389bc74541db50e9a64e1d8829f79a18a
Andreas Åhman <andreas.ahman@ikea.com>
2019-03-06 11:08:44 +0100

    Add getByDays and getNTemps

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
-const endpoint = 'http://schizo.local:3000/';
+const endpoint = environment.endpoint;
+//const endpoint = 'http://localhost:3000/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
@@ -21,6 +23,16 @@ export class RestService {
   }
   getTemps(): Observable<any> {
     return this.http.get(endpoint + 'temps').pipe(
+      map(this.extractData));
+  }
+
+  getLastNTemps(N): Observable<any> {
+    return this.http.get(endpoint + 'temps/limit/' + N).pipe(
+      map(this.extractData));
+  }
+
+  getByDays(days): Observable<any> {
+    return this.http.get(endpoint + 'temps/days/' + days).pipe(
       map(this.extractData));
   }
 
